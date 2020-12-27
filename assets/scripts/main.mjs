78490623867c470b3aad6ecd35cd45d37719e9d6
EFanZh <efanzh@gmail.com>
2020-12-27 20:21:32 +0800

    Try `DocumentFragment`

--- assets/scripts/main.mjs
+++ assets/scripts/main.mjs
@@ -3,44 +3,44 @@ const $ = document.querySelector.bind(document);
 function buildToc(article) {
     const createElement = document.createElement.bind(document);
     const headingLevels = Object.freeze({ "H1": 0, "H2": 1, "H3": 2, "H4": 3, "H5": 4, "H6": 5 });
-
-    function createHeadingAnchor(element) {
-        const result = createElement("a");
-
-        result.href = `#${element.id}`;
-        result.innerText = element.innerText;
-
-        return result;
-    }
-
-    const result = [];
+    const result = new DocumentFragment();
+    const headingAnchors = [];
     const stack = [];
 
     for (const child of article.children) {
         const headingLevel = headingLevels[child.tagName];
 
         if (headingLevel !== undefined) {
-            child.replaceChildren(createHeadingAnchor(child));
-
             const item = createElement("li");
             const details = item.appendChild(createElement("details"));
 
             details.open = true;
-            details.appendChild(createElement("summary")).appendChild(createHeadingAnchor(child));
+
+            const headingAnchor = createElement("a");
+
+            headingAnchor.href = `#${child.id}`;
+            headingAnchor.innerText = child.innerText;
+
+            details.appendChild(createElement("summary")).appendChild(headingAnchor);
             stack[headingLevel] = details;
 
             if (headingLevel === 0) {
-                result.push(item);
+                result.appendChild(item);
             } else {
                 const parent = stack[headingLevel - 1];
-                const children = parent.children[1] || parent.appendChild(createElement("ul"));
 
-                children.appendChild(item);
+                (parent.children[1] || parent.appendChild(createElement("ul"))).appendChild(item);
             }
+
+            headingAnchors.push([child, headingAnchor.cloneNode(true)]);
         }
+    }
+
+    for (const [heading, anchor] of headingAnchors) {
+        heading.replaceChildren(anchor);
     }
 
     return result;
 }
 
-$("aside")?.firstElementChild.lastElementChild.append(...buildToc($("article")));
+$("aside > nav > :last-child")?.appendChild(buildToc($("article")));
