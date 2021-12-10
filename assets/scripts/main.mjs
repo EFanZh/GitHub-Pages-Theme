7731914091dade22119f7855200895cd689dd507
EFanZh <efanzh@gmail.com>
2021-12-11 01:01:42 +0800

    Refactor

--- assets/scripts/main.mjs
+++ assets/scripts/main.mjs
@@ -43,48 +43,48 @@ function buildToc(article) {
     return result;
 }
 
-const sidebar = $("aside");
+const root = $(":root");
+const nav = $("nav");
+const toc = nav.lastElementChild;
+const splitter = nav.nextElementSibling;
 
-if (sidebar) {
-    const root = $(":root");
+toc.append(...buildToc($("article")));
 
-    sidebar.firstElementChild.lastElementChild.append(...buildToc($("article")));
+splitter.addEventListener("pointerdown", function (e0) {
+    const minSidebarWidth = 0;
+    const savedSidebarWidth = nav.offsetWidth - e0.x;
 
-    sidebar.lastElementChild.addEventListener("pointerdown", function (e0) {
-        const minSidebarWidth = 0;
-        const savedSidebarWidth = sidebar.offsetWidth - e0.x;
-
-        if (e0.pointerType === "touch") {
-            function moveHandler(e1) {
-                root.style.setProperty(
-                    "--sidebar-width",
-                    Math.max(savedSidebarWidth + e1.touches[0].clientX, minSidebarWidth) + "px"
-                );
-            }
-
-            function upHandler() {
-                this.removeEventListener("pointerup", upHandler);
-                this.removeEventListener("touchmove", moveHandler);
-            }
-
-            this.addEventListener("touchmove", moveHandler);
-            this.addEventListener("pointerup", upHandler);
-        } else if (e0.button === 0) {
-            const pointerId = e0.pointerId;
-
-            function moveHandler(e1) {
-                root.style.setProperty("--sidebar-width", Math.max(savedSidebarWidth + e1.x, minSidebarWidth) + "px");
-            }
-
-            function upHandler() {
-                this.removeEventListener("pointerup", upHandler);
-                this.removeEventListener("pointermove", moveHandler);
-                this.releasePointerCapture(pointerId);
-            }
-
-            this.setPointerCapture(pointerId);
-            this.addEventListener("pointermove", moveHandler);
-            this.addEventListener("pointerup", upHandler);
+    if (e0.pointerType === "touch") {
+        function moveHandler(e1) {
+            root.style.setProperty(
+                "--sidebar-width",
+                Math.max(savedSidebarWidth + e1.touches[0].clientX, minSidebarWidth) + "px"
+            );
         }
-    });
-}
+
+        function upHandler() {
+            this.removeEventListener("pointerup", upHandler);
+            this.removeEventListener("touchmove", moveHandler);
+        }
+
+        this.addEventListener("touchmove", moveHandler);
+        this.addEventListener("pointerup", upHandler);
+    } else if (e0.button === 0) {
+        const pointerId = e0.pointerId;
+
+        function moveHandler(e1) {
+            root.style.setProperty("--sidebar-width", Math.max(savedSidebarWidth + e1.x, minSidebarWidth) + "px");
+        }
+
+        function upHandler() {
+            this.removeEventListener("lostpointercapture", upHandler);
+            this.removeEventListener("pointermove", moveHandler);
+            this.releasePointerCapture(pointerId);
+        }
+
+        this.setPointerCapture(pointerId);
+        this.addEventListener("pointermove", moveHandler);
+        this.addEventListener("lostpointercapture", upHandler);
+    }
+});
+
